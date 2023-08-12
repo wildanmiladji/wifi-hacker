9c4324513f455b3abcabb2a25bbe7c707e885065
UNknown53 <54210055+wildanmiladji@users.noreply.github.com>
2023-08-12 23:21:45 +0700

    Update page2.js

--- page2.js
+++ page2.js
@@ -1,5 +1,5 @@
 var html =
-  '<h2>Pesan Berhasil Terkirim</h2><h3>Anda akan diarahkan ke halaman utama</h3><h4 class="timer">10</h4><p>Dibuat Oleh :</p><a href="https://youtube.com/@wildanmiladji108" class="yt"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/1280px-YouTube_full-color_icon_%282017%29.svg.png" height="30px" alt="" srcset="" />Wildan mill</a><div class="banner"><p>projek bucin</p><a href="https://youtube.com/@wildanmiladji108"><img src="https://dekatutorial.github.io/18nembak/bannerYT.png" alt="" /></a></div>';
+  '<h2>Pesan Berhasil Terkirim</h2><h3>Anda akan diarahkan ke halaman utama</h3><h4 class="timer">10</h4><p>Dibuat Oleh :</p><a href="https://youtube.com/@wildanmiladji108" class="yt"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/1280px-YouTube_full-color_icon_%282017%29.svg.png" height="30px" alt="" srcset="" />Wildan mill</a><div class="banner"><p>projek bucin</p><a href="https://youtube.com/@wildanmiladji108"><img src="https://google/" alt="" /></a></div>';
 
 document.querySelector("body").innerHTML += html;
 
